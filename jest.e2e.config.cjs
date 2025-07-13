380d2b48bdac4274e8ea0e7581caccbce833bbfa
Dennis Blümer <dennis.bluemer@ducart.de>
2025-07-13 21:00:03 +0200

    Fix babel (#2)
    * Fix babel
    
    * Change babel
    
    * Rename babel
    
    * Change jest
    
    * Fix Playwright
    
    * Fix Playwright
    
    * Fix CI
    
    * Fix CI
    
    * Remove E2E

--- jest.e2e.config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  testEnvironment: 'node',
-  testMatch: ['**/tests/e2e/**/*.test.js'],
-  setupFilesAfterEnv: ['<rootDir>/tests/e2e/setup.js'],
-  testTimeout: 30000,
-  globalSetup: '<rootDir>/tests/e2e/globalSetup.js',
-  globalTeardown: '<rootDir>/tests/e2e/globalTeardown.js'
-};
