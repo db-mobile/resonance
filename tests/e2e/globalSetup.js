9d47a4dae61e7f9c03a46baf5d8e54c006a71efc
Dennis Blümer <dennis.bluemer@ducart.de>
2025-07-13 20:57:45 +0200

    Remove E2E

--- tests/e2e/globalSetup.js
+++
@@ -1,4 +0,0 @@
-module.exports = async () => {
-  // Global setup for E2E tests
-  console.log('Setting up E2E test environment...');
-};
--- tests/e2e/globalTeardown.js
+++
@@ -1,4 +0,0 @@
-module.exports = async () => {
-  // Global teardown for E2E tests
-  console.log('Tearing down E2E test environment...');
-};
