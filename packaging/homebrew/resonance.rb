8a3a4be4db42738ee16933ea3bd2e18f1aae6f5b
Dennis Blümer <dennis.bluemer@ducart.de>
2026-04-03 16:33:17 +0200

    Update Homebrew and AUR automatically upon release

---
+++ packaging/homebrew/resonance.rb
@@ -0,0 +1,16 @@
+cask "resonance" do
+  version "CASK_VERSION"
+  sha256 "CASK_SHA256"
+
+  url "https://github.com/db-mobile/resonance/releases/download/v#{version}/Resonance_#{version}_universal.dmg"
+  name "Resonance"
+  desc "A local-first, zero-account API client with excellent user experience"
+  homepage "https://db-mobile.github.io/resonance/"
+
+  app "Resonance.app"
+
+  zap trash: [
+    "~/Library/Application Support/Resonance",
+    "~/Library/Preferences/com.db-mobile.resonance.plist",
+  ]
+end
